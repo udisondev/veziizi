a2f1214ec502e36c5d5169d67969fdf71f23fcbc
Sergei <s.kanaev@postgrespro.ru>
2026-04-12 09:35:21 +0400

    vite host

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, fileURLToPath(new URL('..', import.meta.url)), '')
 
   return {
     plugins: [vue(), tailwindcss()],
